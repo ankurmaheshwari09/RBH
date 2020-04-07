f561f7d0ff89ae522dbbf0c3291c86f0f2e27e48
A.H.V.Sahithi Reddy <30788772+SahithiReddy7@users.noreply.github.com>
2020-04-07 12:29:01 +0530

    intgrated education and result code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,41 +1,43 @@
-import  React, { Component } from 'react';
-import { Alert, View, StyleSheet, Button, Text} from 'react-native';
+import React, { Component } from 'react';
+import { Alert, View, StyleSheet, Button, Text } from 'react-native';
 import { CustomHeader } from '../components/CustomHeader';
-import {globalStyles} from '../styles/global';
-import {generalInfoConstants} from '../constants/GeneralInfoConstants';
+import { globalStyles } from '../styles/global';
+import { generalInfoConstants } from '../constants/GeneralInfoConstants';
 import { statusConstants } from '../constants/StatusConstants';
+import { educationConstants } from '../constants/EducationConstants';
 
 
 export default class HomeScreen extends Component {
 
-displayHealthRemainder = () => {
-    var month = new Date().getMonth() + 1; //Current Month
-    if(month == 1 || month == 4 || month == 7 || month == 10){
-        Alert.alert(
-          'Health Assessment Remainder',
-          ' Do the Health-Checkup',
-          [
-            {
-              text: 'Cancel',
-              onPress: () => console.log('Cancel Pressed'),
-            },
-            {text: 'OK', onPress: () => console.log('OK Pressed')},
-          ],
-          {cancelable: false},
-        );
-      }
-}
+    displayHealthRemainder = () => {
+        var month = new Date().getMonth() + 1; //Current Month
+        if (month == 1 || month == 4 || month == 7 || month == 10) {
+            Alert.alert(
+                'Health Assessment Remainder',
+                ' Do the Health-Checkup',
+                [
+                    {
+                        text: 'Cancel',
+                        onPress: () => console.log('Cancel Pressed'),
+                    },
+                    { text: 'OK', onPress: () => console.log('OK Pressed') },
+                ],
+                { cancelable: false },
+            );
+        }
+    }
 
     componentDidMount() {
         generalInfoConstants()
         statusConstants()
-      this.displayHealthRemainder();
+        educationConstants()
+        this.displayHealthRemainder();
     }
 
-    render () {
+    render() {
         return (
             <View style={globalStyles.homeView}>
-                <Button style={globalStyles.addChildBtn} title='Add Child' onPress = {() => this.props.navigation.navigate('AddChild')}></Button>
+                <Button style={globalStyles.addChildBtn} title='Add Child' onPress={() => this.props.navigation.navigate('AddChild')}></Button>
                 <View>
                     <Text style={globalStyles.homeScreenText}>Rainbow Homes Child Reporting Tool</Text>
                 </View>
