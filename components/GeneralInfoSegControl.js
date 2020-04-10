c8dc306088898fa2a3151e4f39a47e287783d10e
Kunda Rama Krishna <kundaramakrishna@Kundas-MacBook-Pro.local>
2020-04-10 22:03:05 +0530

    resolved issue in general info for newly added child

--- components/GeneralInfoSegControl.js
+++ components/GeneralInfoSegControl.js
@@ -15,42 +15,42 @@ import {LoadingDisplay} from '../utils/LoadingDisplay';
 export default class GeneralInfoSegControl extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      formIndex: 0,
-      loading: false,
-      child: this.props.navigation.getParam('child'),
-      childHealth: {
-        "bloodGroup": '',
-        "generalHealth": '',
-        "height": '',
-        "weight": '',
-        "comments": '',
-        "newChild": true
-      },
-      prevEducation: {
-        "dropoutReason": '',
-        "date_to": '',
-        "yearOfStudied": '',
-        "medium": '',
-        "schoolName": '',
-        "schooltype": '',
-        "studyingclass": '',
-        "address": '',
-        "newChild": true
-      },
-      childData: []
-    }
+  }
+  state = {
+    formIndex: 0,
+    loading: false,
+    child: this.props.navigation.getParam('child'),
+    childHealth: {
+      "bloodGroup": '',
+      "generalHealth": '',
+      "height": '',
+      "weight": '',
+      "comments": '',
+      "newChild": true
+    },
+    prevEducation: {
+      "dropoutReason": '',
+      "date_to": '',
+      "yearOfStudied": '',
+      "medium": '',
+      "schoolName": '',
+      "schooltype": '',
+      "studyingclass": '',
+      "address": '',
+      "newChild": true
+    },
+    childData: []
   }
 
     componentDidMount(){
       this.setState({ search: null, loading: true });
       getDataAsync(base_url + '/child/' + this.state.child.childNo).then(data => this.setState({childData: data}))
       getDataAsync(base_url + '/child-health-all-records/' + this.state.child.childNo).then(data => {
-        if(data !== [] && data !== null)
+        if(JSON.stringify(data) !== JSON.stringify([]) && data !== null)
           this.setState({childHealth: data[0]})
         })
       getDataAsync(base_url + '/child-education/' + this.state.child.childNo).then(data => {
-        if(data !== null && data !== [])
+        if(data !== null && JSON.stringify(data) !== JSON.stringify([]))
           this.setState({prevEducation: data[0]})
         this.setState({loading: false})
       })
