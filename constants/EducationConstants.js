b9b5b89e3c0472528016a95dde413b7a2ecd9b12
A.H.V.Sahithi Reddy <30788772+SahithiReddy7@users.noreply.github.com>
2020-04-25 19:58:23 +0530

    APIs integrated

--- constants/EducationConstants.js
+++ constants/EducationConstants.js
@@ -3,13 +3,19 @@ import { base_url, getDataAsync } from './Base'
 export function educationConstants() {
 
     global.studyingclass;
-    getDataAsync(base_url + '/studying-class').then(data => {  global.studyingclass = data })
+    getDataAsync(base_url + '/studying-class').then(data => { global.studyingclass = data })
 
     global.medium;
     getDataAsync(base_url + '/mother-tongues').then(data => { global.medium = data })
 
     global.schooltype;
     getDataAsync(base_url + '/school-type').then(data => { global.schooltype = data })
+
+    global.childstaytype;
+    getDataAsync(base_url + '/childstay-type').then(data => { global.childstaytype = data })
+
+    global.scholorshiptype;
+    getDataAsync(base_url + '/scholarship-type').then(data => { global.scholorshiptype = data })
 
 
 }
