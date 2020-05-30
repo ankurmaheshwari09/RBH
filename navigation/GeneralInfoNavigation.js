7d08180ac8d84a09d1467087891f2d4f490dfbb3
Kunda Rama Krishna <kundaramakrishna@Kundas-MacBook-Pro.local>
2020-05-30 10:29:18 +0530

    removed InfoGeneral screen

--- navigation/GeneralInfoNavigation.js
+++ navigation/GeneralInfoNavigation.js
@@ -2,15 +2,11 @@ import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation'
 import {React} from 'react';
 import GeneralInfoSegControl from '../components/GeneralInfoSegControl'
-import InfoGeneral from '../screens/InfoGeneral';
 
 const screens = {
     Modify: {
         screen: GeneralInfoSegControl
-    },
-    InfoGeneral: {
-        screen: InfoGeneral
-    },
+    }
 }
 
 const HomeStack = createStackNavigator(screens, {
--- navigation/StackNavigation.js
+++ navigation/StackNavigation.js
@@ -4,41 +4,16 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation'
 
 import EducationScreen from './EducationNavigation';
-import StatusScreen from '../screens/StatusScreen';
-import HealthScreen from '../screens/HealthScreen';
 import ChildList from '../components/ChildList';
 import ViewProfile from '../components/ViewProfile';
-import ProfileNav from './ProfileNav';
-import GeneralInfoForm from '../components/GeneralInfoForm';
-import InfoGeneral from '../screens/InfoGeneral';
 import GeneralInfoStack from './GeneralInfoNavigation';
-//import CommitteeScreen from '../screens/CommitteeScreen';
 import CommitteeSuggestionNavigation from './CommitteeSuggestionNavigation';
 import StatusNavigator from './StatusNavigator';
-import FollowUpScreen from '../screens/FollowUpScreen';
 import FollowUpNavigation from './FollowUpNavigation';
-import FamilyScreen from '../screens/FamilyScreen';
-import CommunicationScreen from '../screens/CommunicationScreen';
 import FamilyInfoNavigation from './FamilyInfoNavigation';
 import CommunicationInfoNavigation from './CommunicationInfoNavigation';
 import childResultScreen from './ChildResultNavigation';
 import HealthInfoNavigation from './HealthInfoNavigation';
-
-// const Stack = createStackNavigator();
-// export default function StackNavigation() {
-//     return (
-//         <NavigationContainer>
-//             <Stack.Navigator>
-//                 <Stack.Screen name="ChildList" component={ChildList} />
-//                 <Stack.Screen name="About" component={EducationScreen} />
-//                 <Stack.Screen name="Status" component={StatusScreen} />
-//                 <Stack.Screen name="Health" component={HealthScreen} />
-//             </Stack.Navigator>
-//         </NavigationContainer>
-//     );
-// }
-
-
 
 const screens = {
     ViewChild: {
